96fa53aa95c816c38d1ca5c41c0487c31176fe97
Adamprimus <59438710+Adamprimus@users.noreply.github.com>
2022-01-20 00:44:27 -0500

    Add files via upload

---
+++ ClassRegistrationDjango-main/ClassRegistrationSite/ClassRegistrationSite/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.forms import UserCreationForm
+
+from django.db import models
+from django import forms
+
+
+MAJOR_CHOICES = [
+    ("CISC", "Computer Science"),
+    ("CYBSEC", "Cyber Security"),
+    ("MATH", "Mathematics"),
+    ("PHYS", "Physics"),
+    ("CHEM", "Chemistry"),
+    ("BIO", "Biology"),
+    ("PSYC", "Psychology"),
+    ("REL", "Religion"),
+    ("ECON", "Economy"),
+    ("ACCO", "Accounting"),
+    ("BUSN", "Business"),
+    ("ART", "Arts and Crafts"),
+    ("EXSC", "Exercise Science"),
+    ("INTR", "International Relations"),
+    ("POLS", "Political Science"),
+]
+
+class Student(models.Model):
+    fullName = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    expectedGradDate = models.IntegerField(max_length=50)
+
+    def __str__(self):
+        return self.fullName
+
+class Class(models.Model):
+    classId = models.IntegerField(max_length=50)
+    className = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.classId
+
+
+
+
+
+
+
+
+
+
